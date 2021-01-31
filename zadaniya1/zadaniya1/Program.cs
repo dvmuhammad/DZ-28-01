b9848a1607bbd2e64d16a95e10b33ee8a898094c
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-31 20:35:04 +0500

    Add zadaniya1/

---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace zadaniya1
+{
+    class ArrayHelper
+	{
+		public static void Pop()
+		{
+			Random random = new Random();
+			int[] a = Enumerable.Repeat(0, 15).Select(x => random.Next(0, 10)).ToArray();
+			Console.WriteLine(string.Join(" ", a.Select(x => x.ToString()).ToArray()));
+
+			System.Array.Resize(ref a, a.Length - 1);
+			Console.WriteLine(string.Join(" ", a.Select(x => x.ToString()).ToArray()));
+			Console.ReadKey(true);
+		}
+
+		public static void Push(ref int[] array, int value, int index)
+		{
+			
+
+			int[] newArray = new int[array.Length + 1];
+			newArray[index] = value;
+
+			for (int i = 0; i < index; i++)
+				newArray[i] = array[i];
+
+			for (int i = index; i < array.Length;i++)
+				newArray[i+1] = array[i];
+
+			array = newArray;
+			Console.WriteLine(newArray);
+				
+		}
+		public static void Shift()
+		{
+
+		}
+		public static void Unshift(ref int[] array, int value)
+		{
+			Push(ref array, value,0);
+
+
+		}
+
+	}
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int [] myArray = { 1, 4, 6, 2 };
+			ArrayHelper.Push(ref myArray, -5, 2);
+
+			ArrayHelper.Unshift(ref myArray, -5);
+
+			ArrayHelper araryHelper = new ArrayHelper();
+
+			ArrayHelper.Pop();
+
+
+			Console.ReadKey();
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												 
+		}
+	}
+}
+// :((
